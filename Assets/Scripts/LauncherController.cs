c9d52966f40d406d824c919a6f5743fa6f107d1b
lukehinojosa <lukehinojosa05@gmail.com>
2024-11-18 21:35:43 -0500

    Added a Visual Trajectory & Restart Lvl Btn

--- Assets/Scripts/LauncherController.cs
+++ Assets/Scripts/LauncherController.cs
@@ -14,11 +14,26 @@ public class LauncherController : MonoBehaviour
 
     private Queue<GameObject> projectileQueue;
 
-    private float forceMultiplier = 275f;
+    private float forceMultiplier = 5f;
+
+    public GameObject testObject;
+
+    private float dotAmount = 50f;
+    private List<GameObject> dots;
+    List<Vector3> nodes = new List<Vector3>();
+    
     void Start()
     {
         camera = Camera.main;
         projectileQueue = new Queue<GameObject>(projectiles);
+        
+        dots = new List<GameObject>();
+        for (int i = 0; i < dotAmount; i++)
+        {
+            dots.Add(Instantiate(testObject, new Vector3(0f, 0f, 0f), Quaternion.identity));
+            dots[i].SetActive(false);
+        }
+
     }
     
     void Update()
@@ -26,6 +41,7 @@ public class LauncherController : MonoBehaviour
         Controls();
     }
 
+    // ReSharper disable Unity.PerformanceAnalysis
     private void Controls()
     {
         Vector3 camWrldPos = camera.ScreenToWorldPoint(Input.mousePosition);
@@ -55,19 +71,44 @@ public class LauncherController : MonoBehaviour
             dragCircle.SetActive(true);
             
             dragCircle.transform.position = transform.position + new Vector3(direction.x * mouseDistance, direction.y * mouseDistance, 0f);
+            
+            GenerateSamples(dragCircle.transform.position, -direction, mouseDistance * forceMultiplier, dotAmount, Time.fixedDeltaTime);
+
+            for (int i = 0; i < dots.Count; i += 2)
+            {
+                dots[i].SetActive(true);
+                dots[i].transform.position = nodes[i];
+            }
         }
 
         if (dragging && Input.GetMouseButtonUp(0))
         {
+            for (int i = 0; i < dots.Count; i += 2)
+                dots[i].SetActive(false);
+            
             dragCircle.SetActive(false);
             dragging = false;
             
             projectileQueue.Peek().gameObject.transform.SetParent(null);
             projectileQueue.Peek().gameObject.GetComponent<Rigidbody>().useGravity = true;
-            projectileQueue.Peek().gameObject.GetComponent<Rigidbody>().AddForce(-direction * (mouseDistance * forceMultiplier));
+            projectileQueue.Peek().gameObject.GetComponent<Rigidbody>().AddForce(-direction * (mouseDistance * forceMultiplier), ForceMode.Impulse);
             projectileQueue.Peek().gameObject.GetComponent<Rigidbody>().excludeLayers = LayerMask.GetMask();
             
             projectileQueue.Dequeue();
+        }
+    }
+    
+    void GenerateSamples(Vector3 origin, Vector2 direction, float strength, float samples, float deltaT)
+    {
+        nodes.Clear();
+        for (int i = 0; i < samples; i++)
+        {
+            Vector3 pos;
+            pos.x = origin.x + direction.x * strength * i * deltaT;
+            pos.y = origin.y + direction.y * strength * i * deltaT +
+                    0.5f * Physics2D.gravity.y * i * i * deltaT * deltaT;
+            pos.z = 0f;
+            nodes.Add(pos);
         }
     }
 }
